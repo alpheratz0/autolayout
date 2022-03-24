d80c32757d914d3bf2d784d2104b9ba3863f1f53
alpheratz0 <alpheratz99@protonmail.com>
2022-03-24 12:10:21 -0300

    style: move typedefs to the top

--- i3.h
+++ i3.h
@@ -7,14 +7,14 @@
 #define I3_WEVCH_FOCUS 				((1) << 1)
 #define I3_WEVCH_MOVE 				((1) << 2)
 
+typedef int i3_connection_t;
+typedef struct i3_window_event i3_window_event_t;
+
 struct i3_window_event {
 	i32 change;
 	i32 width;
 	i32 height;
 };
-
-typedef int i3_connection_t;
-typedef struct i3_window_event i3_window_event_t;
 
 extern i3_connection_t
 i3_connect(void);
