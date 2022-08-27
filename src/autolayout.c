cb4d1428dd717ca841bb3280e074ef95bd9f0a17
alpheratz0 <alpheratz99@protonmail.com>
2022-08-26 21:50:00 -0300

    do not include unnecessary headers

--- src/autolayout.c
+++ src/autolayout.c
@@ -44,12 +44,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include <signal.h>
 #include <sys/stat.h>
-#include <sys/resource.h>
-#include <sys/types.h>
 
 #include "i3.h"
 #include "debug.h"
