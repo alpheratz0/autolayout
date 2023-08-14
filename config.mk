7e2e37aa3330d774603ca41a2b511ad43ba70f41
alpheratz0 <alpheratz99@protonmail.com>
2023-08-13 22:47:57 -0300

    format config.mk

--- config.mk
+++ config.mk
@@ -1,12 +1,10 @@
 # Copyright (C) 2022-2023 <alpheratz99@protonmail.com>
 # This program is free software.
 
-VERSION   = 0.1.0
-
-CC        = cc
-CFLAGS    = -std=c99 -pedantic -Wall -Wextra -Os -DVERSION=\"$(VERSION)\"
-LDLIBS    = -ljson-c
-LDFLAGS   = -s
-
-PREFIX    = /usr/local
-MANPREFIX = $(PREFIX)/share/man
+VERSION=0.1.0
+CC=cc
+CFLAGS=-std=c99 -pedantic -Wall -Wextra -Os -DVERSION=\"$(VERSION)\"
+LDLIBS=-ljson-c
+LDFLAGS=-s
+PREFIX=/usr/local
+MANPREFIX=$(PREFIX)/share/man
